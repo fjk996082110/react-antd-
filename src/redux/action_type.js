a6a18a7618efac8a46020706e4eac6e23aa38aed
[fengjiankai] <[fjklool@163.com]>
2021-01-07 14:20:12 +0800

    其他

---
+++ src/redux/action_type.js
@@ -0,0 +1,4 @@
+export const SAVE_USER_INFO = 'save_user_info'
+export const DELETE_USER_INFO = 'delete_user_info'
+export const SAVE_MENU_TITLE = 'save_menu_title'
+export const DELETE_MENU_TITLE = 'delete_menu_title'
